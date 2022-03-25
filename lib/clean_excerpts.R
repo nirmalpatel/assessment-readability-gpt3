abb5bfe6f17dc935e85dc72d28267d8acce83a6a
Nirmal Patel <nirmal@playpowerlabs.com>
2022-03-25 23:46:11 +0530

    Data for paper

--- lib/clean_excerpts.R
+++
@@ -1,7 +0,0 @@
-clean_excerpts <- function(excerpts) {
-  # first we clean the input excerpt
-  excerpts %>%
-    mutate(excerpt = str_replace(excerpt, "^[0-9]+\\.|, ", ""),
-           excerpt = str_replace(excerpt, "^ ", ""),
-           excerpt = sapply(excerpt, fix_first_word_if_possible))
-}
